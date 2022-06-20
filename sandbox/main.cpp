ec619a6d7a40cf7257b78cd6766402d5a25c5a09
Artem Vyazemskiy <artvyz17@yandex.ru>
2022-06-20 12:18:56 +0300

    Перебор всех ребер отверстий

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <conio.h>
 
 #include "kompasUtils.hpp"
 
@@ -7,8 +8,8 @@
 #import "kAPI5.tlb" no_namespace named_guids rename( "min", "Imin" ) rename( "max", "Imax" ) rename( "ksFragmentLibrary", "ksIFragmentLibrary" )
 #import "kAPI7.tlb" no_namespace named_guids rename( "CreateWindow", "ICreateWindow" ) rename( "PostMessage", "IPostMessage" ) rename( "MessageBoxEx", "IMessageBoxEx" )
 
-
-int main() {
+// Перебираем все ребра
+int main1() {
     CoInitialize(nullptr);
     KompasObjectPtr kompas = kompasInit();
 
@@ -23,35 +24,84 @@ int main() {
     ksBodyPtr body = part->GetMainBody();
     ksFaceCollectionPtr faces = body->FaceCollection();
     int facesCount = faces->GetCount();
-    for (int i = 0; i < facesCount; i++) {
-        ksFaceDefinitionPtr face = faces->GetByIndex(i);
+    for (int k = 0; k < facesCount; k++) {
+        ksFaceDefinitionPtr face = faces->GetByIndex(k);
         ksLoopCollectionPtr loops(face->LoopCollection());
-        chooseMng->Choose(face);
-        kompas->ksMessage("face");
-        chooseMng->UnChooseAll();
-
+        _getch();
+        chooseMng->UnChooseAll(); chooseMng->Choose(face);
+        std::cout << "face; skip? [s]\n";
+        if (_getch() == 's') {
+            continue;
+        }
 
         for (int i = 0; i < loops->GetCount(); i++) {
             ksLoopPtr loop(loops->GetByIndex(i));
-
-            chooseMng->Choose(loop);
+            chooseMng->UnChooseAll(); chooseMng->Choose(loop);
             if (loop->IsOuter()) {
-                kompas->ksMessage("outer loop");
+                std::cout << "    outer loop " << i << "\n";
             }
             else {
-                kompas->ksMessage("NOT outer loop");
+                std::cout << "    NOT outer loop " << i << "\n";
             }
 
-            chooseMng->UnChooseAll();
-
-
             ksEdgeCollectionPtr edges(loop->EdgeCollection());
-            for (int i = 0; i < edges->GetCount(); i++) {
-                ksEdgeDefinitionPtr edge(edges->GetByIndex(i));
-                chooseMng->Choose(edge);
-                kompas->ksMessage("edge");
-                chooseMng->UnChooseAll();
+            for (int j = 0; j < edges->GetCount(); j++) {
+
+                ksEdgeDefinitionPtr edge(edges->GetByIndex(j));
+                _getch();
+                chooseMng->UnChooseAll(); chooseMng->Choose(edge);
+                std::cout << "        edge " << j << "\n";
+                _getch();
+                chooseMng->UnChooseAll(); chooseMng->Choose(edge->GetAdjacentFace(true));
+                std::cout << "            AdjacentFace(true)" << "\n";
+                _getch();
+                chooseMng->UnChooseAll(); chooseMng->Choose(edge->GetAdjacentFace(false));
+                std::cout << "            AdjacentFace(false)" << "\n";
             }
         }
     }
+    return 0;
+}
+
+// Перебираем все рабра отверстий
+int main() {
+    CoInitialize(nullptr);
+    KompasObjectPtr kompas = kompasInit();
+
+    IApplicationPtr api7 = kompas->ksGetApplication7();
+    IKompasDocument3DPtr document3d(api7->GetActiveDocument());
+    IPart7Ptr topPart(document3d->GetTopPart());
+
+    ksDocument3DPtr doc3d = kompas->ActiveDocument3D();
+    ksChooseMngPtr chooseMng(doc3d->GetChooseMng());
+
+    ksPartPtr part = kompas->TransferInterface(topPart, 1, 0);
+    ksBodyPtr body = part->GetMainBody();
+
+    ksFaceCollectionPtr faces = body->FaceCollection();
+    int facesCount = faces->GetCount();
+    for (int k = 0; k < facesCount; k++) {
+        ksFaceDefinitionPtr face = faces->GetByIndex(k);
+        if (!face->IsPlanar()) {
+            continue;
+        }
+
+        ksLoopCollectionPtr loops(face->LoopCollection());
+        for (int i = 0; i < loops->GetCount(); i++) {
+            ksLoopPtr loop(loops->GetByIndex(i));
+            if (loop->IsOuter()) {
+                continue;
+            }
+
+            ksEdgeCollectionPtr edges(loop->EdgeCollection());
+            for (int j = 0; j < edges->GetCount(); j++) {
+                ksEdgeDefinitionPtr edge(edges->GetByIndex(j));
+                if (face != edge->GetAdjacentFace(true)) {
+                    continue;
+                }
+                chooseMng->Choose(edge);
+            }
+        }
+    }
+    return 0;
 }
